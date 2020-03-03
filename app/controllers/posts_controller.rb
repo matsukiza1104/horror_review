35573a10ef4873c78efc468daf55389d33074af5
matsukiza1104 <matsukiza1104@gmail.com>
2020-03-03 19:15:02 +0900

    文字数制限しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,15 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.includes(:images).order("likes_count DESC")
+    # @zombie = @post.where(genre: 'ゾンビ')
+    # @zombie = Post.find_by(genres: [1])
+    # @zombie = @post.post_genres[1]
+    # @zombie = @post.genres
+    # @zombie = Post.genres
+    # @zombie = Genre.includes(:posts).where(id: 1)
+    # binding.pry
+    # @genre = Genre.all
+    # @posts = Post_genre.where(buyer: nil).order(id:“DESC”)
   end
 
   def show
