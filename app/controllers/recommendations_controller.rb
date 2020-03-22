2505b7bfe164f1f98995f694c7ab1d0c0ecb016b
matsukiza1104 <matsukiza1104@gmail.com>
2020-03-22 17:31:38 +0900

    コントローラー　レコメンド

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,4 +8,20 @@ class RecommendationsController < ApplicationController
   def show
   end
 
+  def create
+    @comment = Comment.create(comment_params)
+    @post = Post.find(params[:post_id])
+    if @comment.save
+      redirect_to post_path(@post)
+      flash[:notice] = "🧟‍♀️コメント入力が完了しました🧟‍♂️"
+    else
+      redirect_to post_path(@post)
+      flash[:notice] = "🧟‍♀️コメントが空欄です🧟‍♂️"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
